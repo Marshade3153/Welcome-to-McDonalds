b468331bb1f6fffa35be3c44b29a3cb1406b43e4
Mohammed Sadaf <momosadaf20517@gmail.com>
2024-02-17 13:57:36 -0500

    working

--- main.java
+++ main.java
@@ -1,1 +1,22 @@
-//hello how are you i am under the water
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("========================================");
+            System.out.println("|                                      |");
+            System.out.println("|                                      |");
+            System.out.println("|                                      |");
+            System.out.println("|                                      |");
+            System.out.println("|                                      |");
+            System.out.println("|                                      |");
+            System.out.println("|                                      |");
+            System.out.println("========================================");
+        }
+    }
+
+    public void clear() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+}
